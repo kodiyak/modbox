208ee2441c0397d39f7ce214e944adbb365ce359
kodiyak <mathewsto51@gmail.com>
2025-06-27 21:50:01 -0300

    feat(development): initialize React + TypeScript + Vite setup with ESLint configuration
    - Add .gitignore for development environment
    - Create README.md with project setup instructions
    - Set up ESLint configuration for TypeScript and React
    - Add index.html as entry point for the application
    - Initialize package.json with scripts and dependencies for React, TypeScript, and Vite
    - Create basic App component and main entry file
    - Add TypeScript configuration files for app and node
    - Implement Vite configuration for React with SWC
    - Establish core package with ESLint and TypeScript configurations
    - Develop VirtualFileService for managing virtual file system operations

---
+++ packages/core/src/services/virtual-file-service.ts
@@ -0,0 +1,58 @@
+export type VirtualFile = {
+  type: 'directory';
+  directory: {
+    [key: string]: VirtualFile;
+  }
+} | {
+  type: 'file';
+  content: string;
+}
+
+export class VirtualFileService {
+  private files = new Map<string, VirtualFile>();
+
+  constructor() {
+  }
+
+  private getFile(path: string): VirtualFile | undefined {
+    return this.files.get(path);
+  }
+
+  public async readFile(path: string) {
+    const file = this.getFile(path);
+    return file && file.type === 'file' ? file.content : undefined;
+  }
+
+  public async writeFile(path: string, content: string): Promise<void> {
+    this.files.set(path, {
+      type: 'file',
+      content,
+    });
+  }
+
+  public async rm(path: string) {
+    this.files.delete(path);
+  }
+
+  public async mkdir(path: string): Promise<void> {
+    this.files.set(path, {
+      type: 'directory',
+      directory: {},
+    })
+  }
+
+  public readdir() {
+    const directories: string[] = [];
+    const files: string[] = [];
+
+    for (const [path, file] of this.files.entries()) {
+      if (file.type === 'directory') {
+        directories.push(path);
+      } else if (file.type === 'file') {
+        files.push(path);
+      }
+    }
+
+    return { directories, files };
+  }
+}
