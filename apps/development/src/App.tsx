3f7e1736f45c94a98417d848791175d14c7c9783
kodiyak <mathewsto51@gmail.com>
2025-06-29 03:03:11 -0300

    chore: update development environment with new index.html title, remove React dependencies, and add minimal CSS reset

--- apps/development/src/App.tsx
+++ apps/development/src/App.tsx
@@ -1,5 +1,11 @@
 export default function App() {
-  return (
-    <div>App</div>
-  )
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				background: "#000",
+			}}
+		></div>
+	);
 }
