ee039ccc4eb0e955ba30144a1f84bec69ce3d888
kodiyak <mathewsto51@gmail.com>
2025-06-30 01:43:23 -0300

    fix: remove injected.resolver export from bundler resolvers

--- packages/core/src/services/bundler/polyfill/resolvers/index.ts
+++ packages/core/src/services/bundler/polyfill/resolvers/index.ts
@@ -2,6 +2,5 @@ export * from "./blob.resolver";
 export * from "./cache.resolver";
 export * from "./external.resolver";
 export * from "./graph.resolver";
-export * from "./injected.resolver";
 export * from "./memory.resolver";
 export * from "./virtual.resolver";
